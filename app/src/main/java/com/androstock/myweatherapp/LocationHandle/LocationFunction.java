9076f6beddb460ccc6550fe833b4bbf811e0bedb
Malik <malik96sharfo@hotmail.com>
2019-06-15 02:41:18 +0200

    combined location and weather, both works, but not together --- todo is using the cityname you get from location and apply it to the cityname from weather

--- app/src/main/java/com/androstock/myweatherapp/LocationHandle/LocationFunction.java
+++
@@ -1,6 +0,0 @@
-package com.androstock.myweatherapp.LocationHandle;
-
-public class LocationFunction {
-
-
-}
--- app/src/main/java/com/androstock/myweatherapp/MainActivity.java
+++ app/src/main/java/com/androstock/myweatherapp/MainActivity.java
@@ -1,6 +1,5 @@
 package com.androstock.myweatherapp;
 
-import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
@@ -14,6 +13,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
 import android.view.View;
@@ -23,20 +23,31 @@ import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import org.json.JSONException;
 import org.json.JSONObject;
+
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
 
-public class MainActivity extends AppCompatActivity implements LocationListener {
+public class MainActivity extends AppCompatActivity implements LocationListener{
 
+    //Loacation related
     Button getLocationBtn;
     TextView locationText;
-
     LocationManager locationManager;
+
+    //weather related
+    TextView selectCity, cityField, detailsField, currentTemperatureField, humidity_field, pressure_field, weatherIcon, updatedField;
+    ProgressBar loader;
+    Typeface weatherFont;
+    //String city = "Barcelona, ES";
+    String cityName = "";
+    String city = cityName;
+    String OPEN_WEATHER_MAP_API = "34910ce547d9588c2b86389c14d95e5c";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,9 +55,9 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
 
+        //Location related
         getLocationBtn = (Button)findViewById(R.id.getLocationBtn);
         locationText = (TextView)findViewById(R.id.locationText);
-
         if (ContextCompat.checkSelfPermission(getApplicationContext(),
                 android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
                 && ActivityCompat.checkSelfPermission(getApplicationContext(),
@@ -62,8 +73,56 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
                 getLocation();
             }
         });
+
+
+        //weather related
+        loader = (ProgressBar) findViewById(R.id.loader);
+        selectCity = (TextView) findViewById(R.id.selectCity);
+        cityField = (TextView) findViewById(R.id.city_field);
+        updatedField = (TextView) findViewById(R.id.updated_field);
+        detailsField = (TextView) findViewById(R.id.details_field);
+        currentTemperatureField = (TextView) findViewById(R.id.current_temperature_field);
+        humidity_field = (TextView) findViewById(R.id.humidity_field);
+        pressure_field = (TextView) findViewById(R.id.pressure_field);
+        weatherIcon = (TextView) findViewById(R.id.weather_icon);
+        weatherFont = Typeface.createFromAsset(getAssets(), "fonts/weathericons-regular-webfont.ttf");
+        weatherIcon.setTypeface(weatherFont);
+
+        taskLoadUp(city);
+
+        selectCity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
+                alertDialog.setTitle("Change City");
+                final EditText input = new EditText(MainActivity.this);
+                input.setText(city);
+                LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.MATCH_PARENT);
+                input.setLayoutParams(lp);
+                alertDialog.setView(input);
+
+                alertDialog.setPositiveButton("Change",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                city = input.getText().toString();
+                                taskLoadUp(city);
+                            }
+                        });
+                alertDialog.setNegativeButton("Cancel",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+                alertDialog.show();
+            }
+        });
     }
 
+
+    //Location related
     void getLocation() {
         try {
             locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
@@ -76,13 +135,21 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
 
     @Override
     public void onLocationChanged(Location location) {
-        locationText.setText("Latitude: " + location.getLatitude() + "\n Longitude: " + location.getLongitude());
+        locationText.setText("Latitude: " + location.getLatitude()
+                + "\n Longitude: " + location.getLongitude());
 
         try {
-            Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+            /*Geocoder geocoder = new Geocoder(this, Locale.getDefault());
             List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
-            locationText.setText(locationText.getText() + "\n"+addresses.get(0).getAddressLine(0)+", "+
-                    addresses.get(0).getAddressLine(1)+", "+addresses.get(0).getAddressLine(2));
+            cityName = addresses.get(0).getAddressLine(0);
+            locationText.setText(cityName);*/
+            Geocoder gcd = new Geocoder(this, Locale.getDefault());
+            List<Address> addresses = gcd.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+            //if (addresses.size() > 0)
+            cityName = addresses.get(0).getLocality();
+                locationText.setText(addresses.get(0).getLocality());
+            //locationText.setText(locationText.getText() + "\n"+addresses.get(0).getAddressLine(0) /*+ ", "+
+              //      addresses.get(0).getAddressLine(1)+", "+addresses.get(0).getAddressLine(2) */ );
         }catch(Exception e)
         {
 
@@ -102,6 +169,59 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     @Override
     public void onProviderEnabled(String provider) {
 
+    }
+
+
+    //weather related
+    public void taskLoadUp(String query) {
+        if (Function.isNetworkAvailable(getApplicationContext())) {
+            DownloadWeather task = new DownloadWeather();
+            task.execute(query);
+        } else {
+            Toast.makeText(getApplicationContext(), "No Internet Connection", Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+    class DownloadWeather extends AsyncTask<String, Void, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            loader.setVisibility(View.VISIBLE);
+
+        }
+
+        protected String doInBackground(String... args) {
+            return Function.excuteGet("http://api.openweathermap.org/data/2.5/weather?q=" + args[0] +
+                    "&units=metric&appid=" + OPEN_WEATHER_MAP_API);
+        }
+
+        @Override
+        protected void onPostExecute(String xml) {
+
+            try {
+                JSONObject json = new JSONObject(xml);
+                if (json != null) {
+                    JSONObject details = json.getJSONArray("weather").getJSONObject(0);
+                    JSONObject main = json.getJSONObject("main");
+                    DateFormat df = DateFormat.getDateTimeInstance();
+
+                    cityField.setText(json.getString("name").toUpperCase(Locale.US) + ", " + json.getJSONObject("sys").getString("country"));
+                    detailsField.setText(details.getString("description").toUpperCase(Locale.US));
+                    currentTemperatureField.setText(String.format("%.2f", main.getDouble("temp")) + "°");
+                    humidity_field.setText("Humidity: " + main.getString("humidity") + "%");
+                    pressure_field.setText("Pressure: " + main.getString("pressure") + " hPa");
+                    updatedField.setText(df.format(new Date(json.getLong("dt") * 1000)));
+                    weatherIcon.setText(Html.fromHtml(Function.setWeatherIcon(details.getInt("id"),
+                            json.getJSONObject("sys").getLong("sunrise") * 1000,
+                            json.getJSONObject("sys").getLong("sunset") * 1000)));
+
+                    loader.setVisibility(View.GONE);
+                }
+            } catch (JSONException e) {
+                Toast.makeText(getApplicationContext(), "Error, Check City", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
 
